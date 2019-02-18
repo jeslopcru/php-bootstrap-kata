6584798af092db4e33142c89dd9c94dbf58ba8eb
jlopez <jlopez@qashops>
2019-02-18 07:27:52 +0100

    all configured

--- src/GiveAProperName.php
+++ src/GiveAProperName.php
@@ -4,6 +4,9 @@ namespace Katas;
 class GiveAProperName
 {
 
+    /**
+     * @return bool
+     */
     public function giveMeAProperName()
     {
         return true;
--- tests/GiveAProperNameTest.php
+++ tests/GiveAProperNameTest.php
@@ -1,14 +1,16 @@
 <?php
 namespace Katas\Tests;
+
 use Katas\GiveAProperName;
 use PHPUnit\Framework\TestCase;
 
 class GiveAProperNameTest extends TestCase
 {
     /** @test */
-    public function please_rename_me_or_delete_me()
+    public function pleaseRenameMeOrDeleteMe(): void
     {
         $myObject = new GiveAProperName();
         $this->assertTrue($myObject->giveMeAProperName());
     }
 }
+
